88ee89bcdc979bc5abe2633db29213fdb6f78258
Jake Sleman <jakesleman@icloud.com>
2020-05-15 16:46:50 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,7 +1,11 @@
 require 'pry'
 def square_array(num_array)
   new_arr = []
-  num_array.each do 
+  num_array.each do |num|
+    x = num * num 
+    new_arr << x
+    new_arr
+    
    binding.pry
  end
     
