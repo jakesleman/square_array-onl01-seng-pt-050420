455c8215533223503e3077e332f9243a19382869
Jake Sleman <jakesleman@icloud.com>
2020-05-15 16:40:59 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,4 +1,12 @@
-def square_array
-  
+require 'pry'
+def square_array(num_array, element)
+  new_arr = []
+  num_array.each do 
+    x = element * element
+   num_arr << x
+   binding.pry
+ end
+    
+    
   
 end
